f3d7d3f193948369a1662e47ac272408a0425348
Chris Barrett <chris@codesaru.com>
2016-02-14 14:58:11 -0800

    Fix opcode Endianness. Fontset. Few opcodes.
    - Tabs to spaces.
    - Add Chip8 fontset.
    - First few opcodes implemented (0xDXYN only advances pc for the moment).
    - Fixed endianness of opcode retrieval from big-endian program bytecode.

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -4,30 +4,38 @@
 #include <csaru-core-cpp/csaru-core-cpp.h>
 
 struct Chip8 {
-	static const unsigned s_memoryBytes   = 4096;
-	static const unsigned s_registerCount =   16;
-	static const unsigned s_renderWidth   =   64;
-	static const unsigned s_renderHeight  =   32;
-	static const unsigned s_stackSize     =   16;
+    static const unsigned s_memoryBytes   = 4096;
+    static const unsigned s_registerCount =   16;
+    static const unsigned s_renderWidth   =   64;
+    static const unsigned s_renderHeight  =   32;
+    static const unsigned s_stackSize     =   16;
+    static const unsigned s_keyCount      =   16;
 
-	static const uint16_t s_interpreterBegin = 0x000;
-	static const uint16_t s_interpreterEnd   = 0x1FF;
-	static const uint16_t s_fontBegin        = 0x050;
-	static const uint16_t s_fontEnd          = 0x0A0;
-	static const uint16_t s_progRomRamBegin  = 0x200;
-	static const uint16_t s_progRomRamEnd    = 0xFFF;
+    static const uint16_t s_interpreterBegin = 0x000;
+    static const uint16_t s_interpreterEnd   = 0x1FF;
+    static const uint16_t s_fontBegin        = 0x050;
+    static const uint16_t s_fontEnd          = 0x0A0;
+    static const uint16_t s_progRomRamBegin  = 0x200;
+    static const uint16_t s_progRomRamEnd    = 0xFFF;
+
+    uint8_t  m_memory[s_memoryBytes];
+    uint8_t  m_v[s_registerCount]; // registers V1-VF
+    uint16_t m_i;  // index register
+    uint16_t m_pc; // program counter
+    uint8_t  m_delayTimer; // decrement if not 0
+    uint8_t  m_soundTimer; // decrement if not 0; beep at 0
+    uint8_t  m_keyStates[s_keyCount]; // hex keypad buttonstates
 
 	uint16_t m_opcode;
-	uint8_t  m_memory[s_memoryBytes];
-	uint8_t  m_v[s_registerCount]; // registers V1-VF
-	uint16_t m_i;  // index register
-	uint16_t m_pc; // program counter
-	uint8_t  m_delayTimer; // decrement if not 0
-	uint8_t  m_soundTimer; // decrement if not 0; beep at 0
-	uint8_t m_keyStates[s_buttonCount]; // hex keypad buttonstates
+    uint16_t m_stack[s_stackSize];
+    uint8_t  m_sp; // stack pointer
+    uint8_t  m_renderOut[s_renderWidth * s_renderHeight]; // 64x32 B&W display
 
-	uint16_t m_stack[s_stackSize];
-	uint8_t  m_sp; // stack pointer
-	uint8_t  m_renderOut[s_renderWidth * s_renderHeight]; // 64x32 B&W display
+    bool     m_drawFlag; // whether or not a GUI application should render
+
+    void Initialize ();
+    bool LoadProgram (const char * path);
+    void EmulateCycle ();
+    void UpdateKeyStates ();
 };
 
--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,5 +1,128 @@
 // This is *heavily*  based on Laurence Muller's tutorial at
 // http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 
+#include <cstdio>
+#include <cstring>
+
 #include "../include/chip8.hpp"
+
+//=====================================================================
+//
+// Static locals
+//
+//=====================================================================
+
+//=====================================================================
+static const uint16_t s_fontSet[80] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+};
+
+
+//=====================================================================
+//
+// Chip8 definitions
+//
+//=====================================================================
+
+//=====================================================================
+void Chip8::Initialize () {
+
+    m_pc     = s_progRomRamBegin;
+    m_opcode = 0x0000;
+    m_i      = 0x0000;
+    m_sp     = 0x00;
+
+    std::memcpy(m_memory + s_fontBegin, s_fontSet, sizeof(s_fontSet));
+
+}
+
+//=====================================================================
+void Chip8::EmulateCycle () {
+
+    // Fetch opcode
+    m_opcode = m_memory[m_pc] << 8 | m_memory[m_pc + 1];
+    //std::printf("  test 0x%04X\n", m_opcode & 0xF000);
+
+    // Decode opcode
+    if (m_opcode == 0x00E0) { // 0x00E0: clear the screen
+        std::memset(m_renderOut, 0, sizeof(m_renderOut));
+        m_pc += 2;
+    }
+    else if ((m_opcode & 0xF000) == 0x6000) { // 0x6XNN: set VX to NN
+        m_v[ m_opcode & 0x0F00 ] = m_opcode & 0x00FF;
+        m_pc += 2;
+    }
+    else if ((m_opcode & 0xF000) == 0xA000) { // 0xANNN: set I to NNN
+        m_i   = m_opcode & 0x0FFF;
+        m_pc += 2;
+    }
+    else if ((m_opcode & 0xF000) == 0xD000) { // 0xDXYN
+        // XOR-draw N rows of 8-bit-wide sprites from I
+        // at (VX, VY), (VX, VY+1), etc.
+        // VF set to 1 if a pixel is toggled off, otherwise 0.
+        m_pc += 2;
+    }
+    else {
+        std::fprintf(
+            stderr,
+            "Chip8: Bad opcode {0x%04X} at {0x%04X} ({0x%04X}).\n",
+            m_opcode,
+            m_pc,
+            m_pc - s_progRomRamBegin
+        );
+    }
+
+    // Update timers
+    if (m_delayTimer)
+        --m_delayTimer;
+    if (m_soundTimer) {
+        if (!--m_soundTimer) // sound if we just hit 0
+            CSaruCore::Beep(); // beep the PC speaker
+    }
+
+}
+
+//=====================================================================
+bool Chip8::LoadProgram (const char * path) {
+
+    std::FILE * progFile = std::fopen(path, "rb");
+    if (!progFile) {
+        std::fprintf(stderr, "Chip8: Failed to open program file at {%s}.\n", path);
+        return false;
+    }
+
+    std::size_t readCount = std::fread(
+        m_memory + s_progRomRamBegin,
+        1, /* size of element to read (in bytes) */
+        s_progRomRamEnd - s_progRomRamBegin, /* number of element to read */
+        progFile
+    );
+    fclose(progFile);
+    if (!readCount) {
+        std::fprintf(stderr, "Chip8: Failed to read from program file {%s}.\n", path);
+        return false;
+    }
+
+    return true;
+
+}
+
+//=====================================================================
+void UpdateKeyStates () {
+}
 
